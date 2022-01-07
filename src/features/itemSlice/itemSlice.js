c3b410c02495bd8d307a8659db5447dac0a138b7
DaffaAdityaDev <daffaaditya990@gmail.com>
2022-01-07 21:14:51 +0700

    commite

---
+++ src/features/itemSlice/itemSlice.js
@@ -0,0 +1,15 @@
+// import { createSlice } from "@reduxjs/toolkit"
+
+// const initialState = {
+//     items: [ "Solar Panel", "Solar" ]
+// }
+
+// const itemSlice = createSlice({
+//     name: "item",
+//     initialState,
+//     reducers: {}
+// })
+
+// export const selectItem = state => state.item.items
+
+// export default itemSlice.reducer
